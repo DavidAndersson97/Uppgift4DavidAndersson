5c2465fe289e6c13a7b0173d9d087b2c4b9b46da
DavidAndersson97 <davidandersson97@hotmail.com>
2024-10-29 16:58:09 +0100

    Add files via upload

---
+++ Uppgift 4 David Andersson/David Andersson+Helpfunctions/scripts.js
@@ -0,0 +1,7 @@
+
+
+function charRemove(i, hejsan) {
+
+    
+}
+    
---
+++ Uppgift 4 David Andersson/David Andersson+Helpfunctions/scripts1.js
@@ -0,0 +1,6 @@
+let cat = { alive: true, 
+            name: "Gustav"}
+
+            function VisaNamnPåKatt() {
+                alert(cat.name)
+            }
